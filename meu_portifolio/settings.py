6d7ff879cea9b3fa19ae9d80180f4430e3bd466b
joao vitor <145628900+desafiogamer@users.noreply.github.com>
2023-11-21 01:04:28 -0300

    Update settings.py

--- meu_portifolio/settings.py
+++ meu_portifolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*50riece%xgo45)5r#@t-_2^4)3yjn^bv4-@-_f2!$$(t3t7vi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
