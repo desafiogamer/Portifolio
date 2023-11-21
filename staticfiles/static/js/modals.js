14f127d4321e97218de492a523076a8eaaae5bce
joao vitor <145628900+desafiogamer@users.noreply.github.com>
2023-11-21 01:27:21 -0300

    Add files via upload

---
+++ staticfiles/static/js/modals.js
@@ -0,0 +1,12 @@
+//janela modal
+
+function abrirModal(){
+    const modal = document.getElementById('janela-modal')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
