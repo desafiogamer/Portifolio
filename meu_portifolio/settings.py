bff9e2656fdf92fbcc23c02616912b02dc288d1d
joao vitor <145628900+desafiogamer@users.noreply.github.com>
2023-11-21 01:39:39 -0300

    Update settings.py

--- meu_portifolio/settings.py
+++ meu_portifolio/settings.py
@@ -119,13 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static") 
 ]
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
-MEDIA_URL = '/uploads/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
