970fc4dfc370e7b8c72767663b45dcf3d75a2855
joao vitor <145628900+desafiogamer@users.noreply.github.com>
2023-11-21 00:47:37 -0300

    Delete paginas directory

--- paginas/admin.py
+++
@@ -1,3 +0,0 @@
-from django.contrib import admin
-from paginas import models
-# Register your models here.
--- paginas/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='index' ),
-    path('send-form-email/', views.SendFormEmail.as_view(), name='send_email'),
-]
